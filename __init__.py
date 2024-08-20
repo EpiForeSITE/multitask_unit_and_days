29a94bfd110910d81d37afd9d3526615ed86803a
George G. Vega Yon <g.vegayon@gmail.com>
2024-08-20 08:51:34 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
-__all__ = ["DataManager", "MultiTaskModel", "run_experiments"]
 
 from multitask_unit_and_days.code import DataManager
 from multitask_unit_and_days.code import MultiTaskModel
 from multitask_unit_and_days.code import run_experiments
 from multitask_unit_and_days.data import *
 
+__all__ = ["DataManager", "MultiTaskModel", "run_experiments"]
 
 # TODO:  pyproject.toml - poetry
 # TODO: Sphinx documentation
